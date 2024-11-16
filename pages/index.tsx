27f4f52b4b013825f061350a84ad0c2d547cbaf6
JMLTUnderCode <juniormiguel08062@gmail.com>
2024-11-16 13:52:16 -0400

    Inclusion de styles CSS.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,23 +1,25 @@
 import { useState } from "react";
 import QueryProcessor from "../utils/QueryProcessor";
+import styles from '../styles/Home.module.css'; // Importa el módulo CSS
 
 export default function Home() {
-  const [query, setQuery] = useState<string>("");
+	const [query, setQuery] = useState<string>("");
 
-  return (
-    <div>
-      <h1>Welcome!!</h1>
-      <p>Please enter your query in the box below:</p>
-      <input
-        type="text"
-        value={query}
-        onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-          setQuery(e.target.value)
-        }
-      />
-      <div>
-        {QueryProcessor(query) == "" ? "No Match" : QueryProcessor(query)}
-      </div>
-    </div>
-  );
+	return (
+		<div className={`${styles.main}`}> {/* Usa la clase del módulo CSS */}
+			<h1 className={`${styles.center}`}>Welcome!!</h1>
+			<p>Please enter your query in the box below:</p>
+			<input
+				type="text"
+				value={query}
+				onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+					setQuery(e.target.value)
+				}
+				className={`${styles.card}`}
+			/>
+			<div className={``}>
+				{QueryProcessor(query) == "" ? "No Match" : QueryProcessor(query)}
+			</div>
+		</div>
+	);
 }
