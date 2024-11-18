dc103b4b0fde0d1c7923a80b6b674c1c1ee0f16e
JMLTUnderCode <juniormiguel08062@gmail.com>
2024-11-18 14:19:51 -0400

    Modificacion de query para juego extreme startup con nueva cuenta.

--- utils/QueryProcessor.tsx
+++ utils/QueryProcessor.tsx
@@ -15,7 +15,7 @@ export default function QueryProcessor(query: string): string {
 	if (query.toLowerCase().includes("name")) {
 		// TODO añade tu USB ID a continuación
 		// TODO actualiza el caso de prueba correspondiente en __tests__
-		return ("JuniorLara");
+		return ("Junior");
 	}
 	return "";
 }
